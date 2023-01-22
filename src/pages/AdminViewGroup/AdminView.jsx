48a5410a0c81f47a10d4504f54a336e5f94cbddc
Anthony Aichouche <anthony.aichouche@student.hogent.be>
2023-01-22 15:19:54 +0100

    deleteopdracht popu bug fix

--- src/pages/AdminViewGroup/AdminView.jsx
+++ src/pages/AdminViewGroup/AdminView.jsx
@@ -248,7 +248,7 @@ const AdminView = () => {
                         groepNaam && (deleteGroup && <DeleteGroupPopup groepNaam={groepNaam.data.naam} groepId={groepNaam.data.id} setDeletePopupGroup={setDeletePopupGroup} navigate={navigate} />)
                     }
                     {
-                        assignmentToDelete && <DeleteOpdrachtPopup assignmentName={assignmentsState[0].naam} opdrachtId={assignmentToDelete} setAssignmentsState={setAssignmentsState} />
+                        assignmentToDelete && <DeleteOpdrachtPopup assignmentToDelete={SetAssignmentToDelete} assignmentName={assignmentsState[0].naam} opdrachtId={assignmentToDelete} setAssignmentsState={setAssignmentsState} />
                     }
                     {
                         subAssignmentToDelete && <DeleteDeelOpdrachtPopup deelOpdrachtId={subAssignmentToDelete} setDeletePopupGroup={SetSubAssignmentToDelete} setAssignmentsState={setAssignmentsState} />
@@ -395,19 +395,20 @@ const DeleteGroupPopup = ({ groepNaam, groepId, setDeletePopupGroup, navigate })
     )
 }
 
-const DeleteOpdrachtPopup = ({ assignmentName, opdrachtId, setAssignmentsState }) => {
+const DeleteOpdrachtPopup = ({ assignmentName, opdrachtId, setAssignmentsState, assignmentToDelete }) => {
     const deleteHandler = (id) => {
         deleteOpdrachtById(id);
         setAssignmentsState([]);
+        assignmentToDelete(false);
     }
 
     return (
         <>
-            <div className="cursor-pointer fixed w-screen top-0 left-0 z-50 p-4 md:inset-0 md:h-full bg-opacity-70 bg-darkGrey flex justify-center items-center" onClick={() => setAssignmentsState([])}>
+            <div className="cursor-pointer fixed w-screen top-0 left-0 z-50 p-4 md:inset-0 md:h-full bg-opacity-70 bg-darkGrey flex justify-center items-center" onClick={() => assignmentToDelete(false)}>
             </div>
             <div className="w-fit fixed left-1/2 bottom-1/2 translate-y-1/2 -translate-x-1/2 z-50 h-fit max-w-md md:h-auto ">
                 <div className="bg-white rounded-lg">
-                    <button onClick={() => setAssignmentsState([])} type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
+                    <button onClick={() => assignmentToDelete(false)} type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
                         <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                         <span className="sr-only">Modal sluiten</span>
                     </button>
@@ -417,7 +418,7 @@ const DeleteOpdrachtPopup = ({ assignmentName, opdrachtId, setAssignmentsState }
                         <button onClick={() => deleteHandler(opdrachtId)} type="button" className="text-white bg-red bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                             Ja, ik ben het zeker
                         </button>
-                        <button onClick={() => setAssignmentsState([])} type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-lightGrey text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Nee, ik wil het niet
+                        <button onClick={() => assignmentToDelete(false)} type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-lightGrey text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Nee, ik wil het niet
                         </button>
                     </div>
                 </div>
